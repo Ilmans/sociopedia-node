e5ac5d3150004524e546733e3d17c3b1ffe6ce2d
Ilman Sunanuddin <ilmansunannudin2@gmail.com>
2023-06-17 10:20:38 +0700

    fix register node js backend

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -27,8 +27,9 @@ export const register = async (req,res) => {
       const savedUser = await newUser.save();
       res.status(201).json(savedUser);
    } catch (error) {
-     res.status(500).json({error : error})
-   }
+     console.log(error);
+     res.status(500).json({ error: error });
+   } 
 }
 
 // LOGGING IN
